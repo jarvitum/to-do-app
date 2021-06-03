2591093fb2e6b38a07122c95391cfa458a1d439f
Javier <84091591+jarvitum@users.noreply.github.com>
2021-06-03 03:03:35 +0300

    Update create-user.component.js

--- mern-todo-app/src/components/create-user.component.js
+++ mern-todo-app/src/components/create-user.component.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 
-export default class CreateExercise extends Component {
+export default class CreateUser extends Component {
   render() {
     return (
       <div>
-        <p>You are on the Create Exercise component!</p>
+        <p>You are on the Create User component!</p>
       </div>
     )
   }
